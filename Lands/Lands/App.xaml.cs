91e1599fc77df2681abc7d51b87bac7e5afcddac
Cristhiam Higuera <higuera.master@outlook.com>
2018-10-31 12:58:45 -0500

    Creo Proyecto

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Lands
+{
+    using Views;
+
+    public partial class App : Application
+    {
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            this.MainPage = new LoginPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart() //Una vez cuando se ejecuta
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() //Cuando la aplicacion se duerme 
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume() //Vuelvo a la aplicación despues de sleep
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
